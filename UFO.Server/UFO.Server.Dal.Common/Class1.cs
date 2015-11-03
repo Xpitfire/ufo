e4d66dfda9632b947b9f2beec2f3c93b4d5f0848
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2015-11-03 12:56:26 +0100

    Created dummy project structure.

---
+++ UFO.Server/UFO.Server.Dal.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFO.Server.Dal.Common
+{
+    public class Class1
+    {
+    }
+}
