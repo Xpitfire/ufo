639eb852bc252443c5d8bf727c38f8b323e15a50
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2015-11-21 18:35:20 +0100

    Added HashCode, ToString and Equals methods for domain objects.

--- UFO.Server/UFO.Server.Domain/Constants.cs
+++ UFO.Server/UFO.Server.Domain/Constants.cs
@@ -17,11 +17,16 @@
 //     Dinu Marius-Constantin
 //     Wurm Florian
 #endregion
+
+using System;
+
 namespace UFO.Server.Domain
 {
     public sealed class Constants
     {
         public const int InvalidIdValue = int.MinValue;
+
+        public const decimal InvalidGeoLocation = decimal.MinValue;
 
         public const string EMailRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
     }
--- UFO.Server/UFO.Server.Domain/Performance.cs
+++ UFO.Server/UFO.Server.Domain/Performance.cs
@@ -29,5 +29,28 @@ namespace UFO.Server.Domain
         public Artist Artist { get; set; }
 
         public Venue Venue { get; set; }
+
+        public override string ToString()
+        {
+            return $"DateTime: {DateTime}, Artist: ({Artist}), Venue: ({Venue})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var performance = obj as Performance;
+            return performance != null
+                && DateTime == performance.DateTime
+                && Artist == performance.Artist
+                && Venue == performance.Venue;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 33;
+            hashCode += DateTime.GetHashCode();
+            hashCode += Venue?.GetHashCode() ?? 0;
+            hashCode += Venue?.GetHashCode() ?? 0;
+            return hashCode;
+        }
     }
 }
