7db8544bee193dc9a45c629b4cd3f92f27cf3248
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2015-12-11 08:52:07 +0100

    Added license header. Moved SessionHandler class to UFO.Server

--- UFO.Server/UFO.Server.Console/Program.cs
+++ UFO.Server/UFO.Server.Console/Program.cs
@@ -32,41 +32,7 @@ namespace UFO.Server
     {
         static void Main(string[] args)
         {
-            var daoFacotry = DalProviderFactories.GetDaoFactory();
-            var artistDao = daoFacotry.CreateArtistDao();
-            artistDao.Update(new Artist())
-                .OnSuccess(artist => Console.WriteLine($"Found artist id: {artist.ArtistId}"))
-                .OnFailure(response => Console.WriteLine($"Error: {response.ErrorMessage}; {response.Exception}"));
 
-            artistDao.SelectAll()
-                .OnSuccess(list =>
-                {
-                    foreach (var artist in list)
-                    {
-                        Console.WriteLine($"{artist}");
-                    }
-                })
-                .OnFailure(response => Console.WriteLine($"Error: {response.ErrorMessage}"));
-
-            var userDao = daoFacotry.CreateUserDao();
-
-            userDao.SelectWhere(users => users.Where(u => u.IsAdmin))
-                .OnSuccess(users =>
-                {
-                    foreach (var user in users)
-                    {
-                        Console.WriteLine($"Update user: ({user}) successful");
-                    }
-                })
-                .OnFailure(response => { throw response.Exception; });
-
-            var performanceDao = daoFacotry.CreatePerformanceDao();
-            performanceDao.SelectById(new DateTime(2015, 11, 15, 22, 00, 00), 64)
-                .OnSuccess(response => Console.WriteLine($"{response.ResultObject}"))
-                .OnEmptyResult(() => Console.WriteLine("No performance data found!"))
-                .OnFailure(response => Console.WriteLine($"{response.Exception}"));
-
-            Console.ReadLine();
         }
     }
 }
